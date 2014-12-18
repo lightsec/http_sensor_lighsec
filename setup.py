03c1c8ab1b5277da5e94f1447631567051e149be
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2014-12-18 17:51:30 +0100

    Minor reformatting issues.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-'''
+"""
 Created on Nov 30, 2014
 
 @author: Aitor Gómez Goiri <aitor.gomez@deusto.es>
@@ -7,23 +7,23 @@ To install/reinstall/uninstall the project and its dependencies using pip:
      pip install ./
      pip install ./ --upgrade
      pip uninstall httpsensorliblightsec
-'''
-from setuptools import setup #, find_packages
+"""
+from setuptools import setup  # , find_packages
 
 setup(name="httpsensorliblightsec",
       version="0.1",
       description="Sample HTTP server for a sensor following the lightsec protocol.",
-      #long_description = "",
-      author = "Aitor Gomez-Goiri",
-      author_email = "aitor.gomez@deusto.es",
-      maintainer = "Aitor Gomez-Goiri",
-      maintainer_email = "aitor.gomez@deusto.es",
-      url = "https://github.com/lightsec/http_sensor_lightsec",
-      #license = "http://www.apache.org/licenses/LICENSE-2.0",
-      platforms = ["any"],
-      package_dir = {
-        '': 'src',
+      # long_description = "",
+      author="Aitor Gomez-Goiri",
+      author_email="aitor.gomez@deusto.es",
+      maintainer="Aitor Gomez-Goiri",
+      maintainer_email="aitor.gomez@deusto.es",
+      url="https://github.com/lightsec/http_sensor_lightsec",
+      # license = "http://www.apache.org/licenses/LICENSE-2.0",
+      platforms=["any"],
+      package_dir={
+          '': 'src',
       },
-      packages = [ "httplightsec" ],
-      install_requires = [ "liblightsec", "Flask" ],
+      packages=["httplightsec"],
+      install_requires=["liblightsec", "Flask"],
 )
